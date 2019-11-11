41788517e92b132cbb24197bf66463c61ffde12b
Meshal Alqahtani <gneral20@gmail.com>
2019-11-11 21:16:14 +0300

    created game logic

---
+++ mem-1/JS/main.js
@@ -0,0 +1,22 @@
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne;
+cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User Flipped Queen");
+
+var cardTwo;
+cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	}
+	else {
+		alert("Sorry, try again.");
+	}
+}
