6246baa54a2ddb1bfe01a3c46b480679430a680e
Meshal Alqahtani <gneral20@gmail.com>
2019-11-12 05:01:36 +0300

    created game logic

--- mem-1/JS/main.js
+++ mem-1/JS/main.js
@@ -1,5 +1,27 @@
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+             {
+             	rank: "queen",
+             	suit: "hearts",
+             	cardImage: "images/queen-of-hearts.png",
+
+             },
+             {
+             	rank: "queen",
+             	suit: "diamonds",
+             	cardImage: "images/queen-of-diamonds.png",
+             },
+             {
+             	rank: "king",
+             	suit: "hearts",
+             	cardImage: "images/king-of-hearts.png",
+             },
+             {
+             	rank: "king",
+             	suit: "diamonds",
+             	cardImage: "images/king-of-diamonds.png",
+             }
+             ];
 var cardsInPlay = [];
 
 
@@ -20,8 +42,10 @@ if (cardsInPlay.length === 2) {
 
 function flipCard(cardId) {
 
-	console.log("User Flipped" + " " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User Flipped" + " " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
 
 checkForMatch();
 
